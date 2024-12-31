3a92838a56ced928f1e8734cf446a0556bf1b0cc
aryajoshi0110 <166370968+aryajoshi0110@users.noreply.github.com>
2024-12-31 23:39:16 +0530

    Update Center_Exchange.py

--- Center_Exchange.py
+++ Center_Exchange.py
@@ -1,5 +1,4 @@
 import vpython as vp
-# from Cube_Piece import Cube_Piece
 from Initialize_Cube import Color_Cube,Mirror_Cube
 from Commands import *
 from time import sleep
@@ -8,17 +7,7 @@ Cube = Color_Cube()
 sleep(5)
 i = 0
 
-fps = 2
-for i in range(1):
-    vp.rate(fps)
-    R(Cube)
-    L_(Cube)
-    F(Cube)
-    B_(Cube)
-    U(Cube)
-    D_(Cube)
-    R(Cube)
-    L_(Cube)
-    i += 1
+for move in [R,L_,F,B_,U,D_,R,L_]:
+    move(Cube)
 
 sleep(10)
